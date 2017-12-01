443f9c036cda1d468364645aebd5698b74641090
Kylep342 <kylep342@gmail.com>
2017-11-30 18:45:55 -0600

    Work on cleanData.py get_pos method created began filter_pos method

--- cleanData.py
+++ cleanData.py
@@ -9,11 +9,17 @@ class cleanTweets(object):
         """
         Instantiating this object escapes
         """
-        self.text = html.escape(input_text)
+        self.text = html.unescape(input_text)
         self.text_list = self.text.split()
         self.word_list = [word for word in self.text_list if
                           word.isalpha()]
+        self.words = " ".join(self.word_list)
         self.hashtags = [word for word in self.text_list if
                          word[0] == '#']
         self.mentions = [word for word in self.text_list if
                          word[0] == '@']
+
+    def get_pos(self, tagset='universal'):
+        if tagset == 'universal':
+            nltk.download('universal_tagset')
+        return nltk.pos_tag(self.word_list, tagset=tagset)
