46a07605d1f14d2066f203c8b8c4fa408e4cb878
Kylep342 <kylep342@gmail.com>
2017-12-04 18:17:01 -0600

    Some cleanup: Added an  file to separate the OAuth config.yaml from the instructions passed to main.py Fleshed out the README a little bit.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from getData import GetTweets
 
 
 def main():
-    cfg = yaml.load(open('config.yaml', 'r'))
-    for handle in cfg['accounts']:
+    instruct = yaml.load(open('instructions.yaml', 'r'))
+    for handle in instruct['accounts']:
         # print(handle)
         AnalyzeTweets(CleanTweets(GetTweets().findTweets(handle)).filter_by_pos('noun')).wordfreqgraph(handle)
 
